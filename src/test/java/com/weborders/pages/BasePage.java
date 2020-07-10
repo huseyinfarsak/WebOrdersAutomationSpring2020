861e1ccf7061efdc944fcedfcd4505e3fadb958a
huseyinfarsak <hfarsak01@hotmail.com>
2020-07-09 22:14:34 -0500

    WebOrders.Java

---
+++ src/test/java/com/weborders/pages/BasePage.java
@@ -0,0 +1,19 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    //explicit wait
+    //will be used inside every page class
+    //since this variable is protected
+    //it will not be visible in test classes
+    protected WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+
+    public BasePage() {
+        //we put this line to be able to use @FindBy, @FindBys.. annotations
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+}
